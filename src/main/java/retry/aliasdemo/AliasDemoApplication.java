93b86328d3d7f31ed68a34e79f0407568e12f428
Henning Poettker <Henning.Poettker@c24.de>
2022-11-17 17:11:25 +0100

    init commit

---
+++ src/main/java/retry/aliasdemo/AliasDemoApplication.java
@@ -0,0 +1,13 @@
+package retry.aliasdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliasDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/retry/aliasdemo/AliasDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package retry.aliasdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliasDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
